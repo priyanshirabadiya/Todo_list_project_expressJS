5a7403ab0fc36b67b6973e7d45da363fbf7b7758
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-09-07 15:28:40 +0530

    7/9/24

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,9 +6,19 @@ const {
     getloginUser,
     userProfile,
     successUser,
+    logoutUser,
     getRegister,
     postLogin
 } = require('../controller/user.controller');
+
+const {
+    otpVerificationalEmail,
+    verifyOTProute,
+    enterEmail,
+    resetPasswordFinally,
+    setNewPassword,
+    getotp
+} = require('../controller/otpverify.controller');
 
 userRoutes.get('/register', getRegister);
 
@@ -17,6 +27,24 @@ userRoutes.get('/login', getloginUser);
 userRoutes.get('/success', successUser);
 
 userRoutes.get('/profile', userProfile);
+
+userRoutes.get('/logout', logoutUser);
+
+// f1 
+// sent otp on email route
+userRoutes.post('/enter-otp', otpVerificationalEmail);
+
+userRoutes.get('/enter-email', enterEmail);
+
+userRoutes.post('/verify-otp', verifyOTProute);
+
+
+
+userRoutes.get("/all-otps" , getotp )
+// f2
+userRoutes.get('/reset-password', resetPasswordFinally);
+
+userRoutes.post('/set-new', setNewPassword);
 
 userRoutes.post('/register', registerUser);
 
